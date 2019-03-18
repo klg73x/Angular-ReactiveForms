5e93a4d754fb8f3e5f46faf1d66e2e7f1f852658
Kevin Gagnon <klg73@outlook.com>
2019-03-18 14:02:03 -0400

    Validation Examples

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+import { patchComponentDefWithScope } from '@angular/core/src/render3/jit/module';
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { 'match': true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +34,56 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: [null,
+        [Validators.required,
+        Validators.minLength(3)]
+      ],
+      lastName: [null,
+        [Validators.required,
+        Validators.maxLength(50)]
+      ],
+      emailGroup: this.fb.group({
+        email: [null,
+          [Validators.required,
+          Validators.email]
+        ],
+        confirmEmail: [null, Validators.required],
+      }, { validator: emailMatcher }),
+      phone: null,
+      notification: 'email',
+      rating: [null, ratingRange(1, 5)],
+      sendCatalog: true
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData() {
+    this.customerForm.setValue({
+      firstName: 'Whatever',
+      lastName: 'Thisis',
+      email: 'wouldiuseit@hellno.com',
+      sendCatalog: false
+    });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
